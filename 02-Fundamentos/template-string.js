6cdab9143dbaba13ba53b2d2e15d01813e820ddb
daniela.gonzalez2 <daniela.gonzalez.m02@mail.pucv.cl>
2023-03-20 12:18:42 -0300

    Sección 2: Fundamentos de Node

---
+++ 02-Fundamentos/template-string.js
@@ -0,0 +1,12 @@
+const nombre = "Deadpool";
+const real = "Wade Winston";
+const normal = nombre + " " + real;
+const template = `${nombre} ${real}`;
+console.log(normal);
+console.log(template);
+console.log(template === normal);
+const html = `
+<h1>Hola</h1>
+<p>Mundo</p>
+`;
+console.log(html);
