1e12a1940b22b4a3e787f48c2e4a3be540bb7fce
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-03-25 11:03:11 -0300

    Sección 2: Fundamentos de Node

--- 02-Fundamentos/promesas.js
+++ 02-Fundamentos/promesas.js
@@ -18,19 +18,32 @@ const getEmpleado = (id) => {
 const getSalario = (id) => {
   const salario = salarios.find((e) => e.id === id)?.salario;
   return new Promise((resolve, reject) => {
-    salario ? resolve(salario) : reject("no existe");
+    salario
+      ? resolve(salario)
+      : reject(`El empleado ${id} no tiene información de salario`);
   });
 };
-const id = 3;
+const id = 34;
 
+// getEmpleado(id)
+//   .then((empleado) => {
+//     getSalario(id)
+//       .then((salario) =>
+//         console.log(`empleado ${empleado} tiene salario: ${salario}`)
+//       )
+//       .catch(() =>
+//         console.log(`El empleado ${empleado} no tiene información de salario`)
+//       );
+//   })
+//   .catch((err) => console.log(err));
+
+let nombre;
 getEmpleado(id)
   .then((empleado) => {
-    getSalario(id)
-      .then((salario) =>
-        console.log(`empleado ${empleado} tiene salario: ${salario}`)
-      )
-      .catch(() =>
-        console.log(`El empleado ${empleado} no tiene información de salario`)
-      );
+    nombre = empleado;
+    return getSalario(id);
   })
+  .then((salario) =>
+    console.log(`empleado ${nombre} tiene salario: ${salario}`)
+  )
   .catch((err) => console.log(err));
