22d9170b683c654f3cd70fb2554cf363e110e940
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-03-27 09:35:51 -0300

    Sección 4: Bases de node

---
+++ 03-bases-node/app.js
@@ -0,0 +1,6 @@
+console.clear();
+console.log("============================");
+const base = 5;
+for (let index = 1; index <= 10; index++) {
+  console.log(`${base} x ${index} = ${base * index}`);
+}
