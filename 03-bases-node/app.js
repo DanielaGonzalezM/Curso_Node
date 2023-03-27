2ec7029c1805f3676428fab1cc6f0c18bd8a89d3
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-03-27 09:56:56 -0300

    Sección 4: Bases de node

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,14 @@
+const fs = require("fs");
 console.clear();
 console.log("============================");
 const base = 5;
+let salida = "";
+const nombreTxt = `tabla-${base}.txt`;
 for (let index = 1; index <= 10; index++) {
-  console.log(`${base} x ${index} = ${base * index}`);
+  salida += `${base} x ${index} = ${base * index} \n`;
 }
+console.log(salida);
+fs.writeFile(nombreTxt, salida, (err) => {
+  if (err) throw err;
+  console.log(`${nombreTxt} creado`);
+});
