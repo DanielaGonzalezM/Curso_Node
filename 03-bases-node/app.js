bb14b4885361261535fce0b0b4349b5dc6378404
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-03-31 10:09:50 -0300

    Sección 4: Bases de node

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,16 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("yargs").argv;
+
+
 
 console.clear();
 
-const [, , arg3 = "base=5"] = process.argv;
-const [, base = 5] = arg3.split("=");
+console.log(process.argv);
+console.log(argv.base);
 
-crearArchivo(base)
-  .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
-  .catch((err) => console.log(err));
+// const [, , arg3 = "base=5"] = process.argv;
+// const [, base = 5] = arg3.split("=");
+
+// crearArchivo(base)
+//   .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
+//   .catch((err) => console.log(err));
