dc55282d9237dcdee11d1dbdd178d808a9606c32
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-05-19 12:36:15 -0400

    Sección 6: Aplicación de Clima - GeoLocation + OpenWeatherMaps

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -8,7 +8,7 @@ class Busquedas {
   async ciudad(lugar = "") {
     try {
       console.log("ciudad", lugar);
-      const resp = await axios.get("https://reqres.in/api/users?page=2");
+      const resp = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/valparaiso.json?proximity=ip&language=es&access_token=MY_TOKEN");
       console.log(resp.data);
       return []; //Retornar los lugares que coincidadn
     } catch (error) {
