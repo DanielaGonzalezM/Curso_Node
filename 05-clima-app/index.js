46c54ce513dca9e965a214cf77bf8e7220cd594b
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-06-08 12:08:28 -0400

    Sección 6: Aplicación de Clima - GeoLocation + OpenWeatherMaps

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -22,14 +22,17 @@ const main = async () => {
         const id = await listarLugares(lugares);
         const lugarSel = lugares.find((l) => l.id === id);
         //clima
+        const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
         //mostrar resultados
+        console.clear();
         console.log("\nInformación de la ciudad\n".green);
         console.log("Ciudad:", lugarSel.nombre);
         console.log("Lat:", lugarSel.lat);
         console.log("Lng:", lugarSel.lng);
-        console.log("Temperatura:");
-        console.log("Mínima:");
-        console.log("Máxima:");
+        console.log("Temperatura:", clima.temp);
+        console.log("Mínima:", clima.min);
+        console.log("Máxima:", clima.max);
+        console.log("Como está el clima: ", clima.desc.green);
         break;
 
       default:
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -11,6 +11,13 @@ class Busquedas {
       language: "es",
     };
   }
+  get paramOpenWeatherMap() {
+    return {
+      appid: process.env.OPENWEATHER_KEY,
+      units: "metric",
+      lang: "es",
+    };
+  }
   async ciudad(lugar = "") {
     try {
       const intance = axios.create({
@@ -25,6 +32,25 @@ class Busquedas {
         lng: lugar.center[0],
         lat: lugar.center[1],
       }));
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async climaLugar(lat, lon) {
+    try {
+      const intance = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`,
+        params: this.paramOpenWeatherMap,
+      });
+      const resp = await intance.get();
+      const { weather, main } = resp.data;
+      return {
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        temp: main.temp,
+      };
     } catch (error) {
       return [];
     }
