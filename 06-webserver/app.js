1e2a8a33fe803aab359fbfde3db867368608dbc9
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-08-17 12:29:26 -0400

    Sección 7: Webserver - HTTP - EXPRESS - HBS

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,7 +2,20 @@ const http = require("http");
 
 http
   .createServer((request, res) => {
-    res.write("HOLA");
+    // res.writeHead(200,{'Conten-Type': 'application/json'});
+    // const persona = {
+    //     id:1,
+    //     nombre: 'Daniela'
+    // }
+    // res.write(JSON.stringify(persona));
+
+    res.setHeader("Content-Disposition", "Attachment; filename=cursolista.csv");
+    res.writeHead(200, { "Conten-Type": "application/csv" });
+    res.write("id, nombre \n");
+    res.write("1, Fernando \n");
+    res.write("2, Daniela \n");
+    res.write("3, Maria \n");
+    res.write("4, Bryam \n");
     res.end();
   })
   .listen(666);
