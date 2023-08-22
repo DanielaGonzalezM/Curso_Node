fa17ff371f9a39bd4cab71ade9cd2c1f87d2df1e
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-08-21 20:42:04 -0400

    Sección 7: Webserver - HTTP - EXPRESS - HBS

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,16 +2,16 @@ const express = require("express")
 const app = express()
 const port = 666
 
-app.get("/", (req,res) => {
-    res.send("Hello World")
-});
+// Servir contenido estático
 
-app.get("/hola-mundo", (req,res) =>{
+app.use(express.static("public"))
+
+ app.get("/hola-mundo", (req,res) =>{
     res.send("Hello World - 2")
 });
 
 app.get("*", (req,res) =>{
-    res.send("404 | Not found")
+    res.sendFile(__dirname + "/public/404.html")
 });
 
 app.listen(port,()=>{
