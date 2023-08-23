2640510b801513ea455c965649c0b0656b78f79c
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-08-23 09:37:00 -0400

    Sección 7: Webserver - HTTP - EXPRESS - HBS

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,23 +1,29 @@
-const express = require("express")
-const app = express()
-const port = 666
+const express = require("express");
+const app = express();
+const port = 666;
 
+//TODO: require("hb")
+app.set("view engine", "hbs");
 // Servir contenido estático
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
- app.get("/generic", (req,res) =>{
-    res.sendFile(__dirname + "/public/generic.html")
+app.get("/", (req, res) => {
+    res.render("home")
+  });
+
+app.get("/generic", (req, res) => {
+  res.sendFile(__dirname + "/public/generic.html");
 });
 
-app.get("/element", (req,res) =>{
-    res.sendFile(__dirname + "/public/element.html")
+app.get("/element", (req, res) => {
+  res.sendFile(__dirname + "/public/element.html");
 });
 
-app.get("*", (req,res) =>{
-    res.sendFile(__dirname + "/public/404.html")
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
 });
 
-app.listen(port,()=>{
-    console.log(`Corriendo en puerto:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Corriendo en puerto:${port}`);
+});
