0a059f3ab996c33d387a4fd8722bdec609c66041
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-09-12 13:59:42 -0300

    Sección 8: REST Server - Configuraciones iniciales

---
+++ 07-restserver/controllers/usuarios.js
@@ -0,0 +1,38 @@
+const { response } = require("express");
+
+const usuariosGet = (req, res = response) => {
+  res.json({
+    msg: "get API - controlador",
+  });
+};
+
+const usuariosPut = (req, res = response) => {
+  res.status(500).json({
+    msg: "put API - controlador",
+  });
+};
+
+const usuariosPost = (req, res = response) => {
+  res.status(201).json({
+    msg: "post API - controlador",
+  });
+};
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    msg: "delete API - controlador",
+  });
+};
+
+const usuariosPatch = (req, res = response) => {
+  res.json({
+    msg: "patch API - controlador",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosDelete,
+  usuariosPost,
+  usuariosPut,
+  usuariosPatch,
+};
--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -5,7 +5,7 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-
+    this.usuariosPath = "/api/usuarios";
     //*Middlewares
     this.middleware();
 
@@ -21,35 +21,7 @@ class Server {
   }
 
   routes() {
-    this.app.get("/api", (req, res) => {
-      res.json({
-        msg: "get API",
-      });
-    });
-
-    this.app.put("/api", (req, res) => {
-      res.status(500).json({
-        msg: "put API",
-      });
-    });
-
-    this.app.post("/api", (req, res) => {
-      res.status(201).json({
-        msg: "post API",
-      });
-    });
-
-    this.app.delete("/api", (req, res) => {
-      res.json({
-        msg: "delete API",
-      });
-    });
-
-    this.app.patch("/api", (req, res) => {
-      res.json({
-        msg: "patch API",
-      });
-    });
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
   }
 
   listen() {
