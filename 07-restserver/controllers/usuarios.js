102a2235a102d7a9fc3f6fd866b1430c8e3070db
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2023-09-18 17:40:56 -0300

    Sección 9: Alcances del RESTServer y mantenimiento

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,4 +1,6 @@
 const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
+
 const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
@@ -21,12 +23,19 @@ const usuariosPut = (req = request, res = response) => {
 };
 
 const usuariosPost = async (req = request, res = response) => {
-  const body = req.body;
-  const usuario = new Usuario(body);
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  //Verificar si el correo existe
+
+  //Encruptar password
+  const salt = bcryptjs.genSaltSync(10);
+  usuario.password = bcryptjs.hashSync(password,salt)
+  //Guardar en BD
   await usuario.save();
   res.status(201).json({
     msg: "post API - controlador",
-    usuario
+    usuario,
   });
 };
 const usuariosDelete = (req = request, res = response) => {
