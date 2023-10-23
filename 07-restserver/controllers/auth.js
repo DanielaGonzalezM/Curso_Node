43fd66906985de328ea58734316b3e111667adfc
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-10-23 10:18:04 -0300

    Sección 10: Autenticación de usuario - JWT

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,8 +1,40 @@
 const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
+const Usuario = require("../models/usuario");
+const login = async (req, res = response) => {
+    const { correo, password } = req.body;
 
-const login = (req = request, res = response) => {
-    res.json({
-        msg: "login ok",
-    });
+    try {
+        //Todo:Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: "Usuario / Password no son correctos - correo",
+            });
+        }
+        //Todo:Si el usuario está activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: "Usuario / Password no son correctos - estado: false",
+            });
+        }
+        //Todo:Verificar la password
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: "Usuario / Password no son correctos - password",
+            });
+        }
+        //Todo:Generar el JWT
+
+        res.json({
+            msg: "login ok",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Hable con el administrador",
+        });
+    }
 };
-module.exports = { login }
+module.exports = { login };
