ba49bf99ee21243e04745f46a5126b51e1aaa507
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2023-10-25 09:50:03 -0300

    Sección 10: Autenticación de usuario - JWT

--- 07-restserver/models/usuario.js
+++ 07-restserver/models/usuario.js
@@ -32,4 +32,9 @@ const UsuarioSchema = Schema({
   },
 });
 
+UsuarioSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...usuario } = this.toObject();
+  usuario.uid = _id;
+  return usuario;
+};
 module.exports = model("Usuario", UsuarioSchema);
