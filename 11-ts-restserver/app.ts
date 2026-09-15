f089ecd08000cdcc9e65e4379c20dc0a86abcf8d
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-15 11:26:27 -0300

    Rutas de la aplicacion

--- 11-ts-restserver/app.ts
+++ 11-ts-restserver/app.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import Server from "./models/server";
 
 //configurar dotenv
+
 dotenv.config();
 
 const server = new Server();
--- 11-ts-restserver/models/server.ts
+++ 11-ts-restserver/models/server.ts
@@ -1,23 +1,28 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
+import userRoutes from "../routes/usuario";
 
-class Server{
+class Server {
+  private app: Application;
+  private port: string;
+  private apiPaths = {
+    usuarios: "/api/usuarios",
+  };
 
-    private app: Application;
-    private port: string;
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || "3000";
+    //definir mis rutas
+    this.route();
+  }
 
-
-    constructor(){
-
-        this.app=express();
-        this.port = process.env.PORT || "3000";
-
-    }
-
-    listen(){
-        this.app.listen(this.port,()=>{
-            console.log("Servidor corriendo en puerto " + this.port)
-        })
-    }
+  route() {
+    this.app.use(this.apiPaths.usuarios, userRoutes);
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto " + this.port);
+    });
+  }
 }
 
 export default Server;
