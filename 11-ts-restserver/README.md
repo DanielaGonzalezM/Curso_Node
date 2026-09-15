ee52c91ca2b2da38e821217fd8d95d044cf6f4e0
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-15 11:35:02 -0300

    Middlewares necesarios

--- 11-ts-restserver/README.md
+++ 11-ts-restserver/README.md
@@ -6,7 +6,7 @@ npm i typescript --save-dev
 ./node_modules/.bin/tslint --init
 npm i express cors dotenv
 npm i --save-dev @types/express
-
+npm i --save-dev @types/cors
 
 
 tsc --watch
--- 11-ts-restserver/models/server.ts
+++ 11-ts-restserver/models/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import userRoutes from "../routes/usuario";
+import cors from "cors";
 
 class Server {
   private app: Application;
@@ -11,13 +12,27 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "3000";
+    //middlewares
+    this.moddleware();
     //definir mis rutas
     this.route();
+  }
+
+  //TODO: Conectar base de datos
+
+  moddleware() {
+    //CORS
+    this.app.use(cors({}));
+    //Lectura de body
+    this.app.use(express.json());
+    //Carpeta publica
+    this.app.use(express.static("public"));
   }
 
   route() {
     this.app.use(this.apiPaths.usuarios, userRoutes);
   }
+
   listen() {
     this.app.listen(this.port, () => {
       console.log("Servidor corriendo en puerto " + this.port);
